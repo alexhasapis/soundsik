765e281ec18bd74bede76bd4228d4c3cce983fa7
Alex Hasapis <alexmhasapis@gmail.com>
2015-03-17 14:03:32 -0400

    fix soundsik name

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -1,8 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-function loadSoundik(){
-  $('<h1>').attr('id', 'title').text('Soundisk').appendTo('div#title');
+function loadSoundsik(){
+  $('<h1>').attr('id', 'title').text('Soundsik').appendTo('div#title');
   $('<h3>').text('Location').appendTo('div#zip-text');
   $('<input>').attr({
     type: 'text',
@@ -12,7 +12,7 @@ function loadSoundik(){
   }).appendTo('div#zip-input');
 }
 
-$(document).ready(loadSoundik);
+$(document).ready(loadSoundsik);
 
 renderGenres(genres);
 renderMoods(moods);
