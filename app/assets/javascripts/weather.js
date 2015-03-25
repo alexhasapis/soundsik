f15063d0f4e7ead3630a8aedb7f144986b0d98f2
Quincy Iheme <qiheme@gmail.com>
2015-03-25 07:29:01 -0400

    added warblr panel

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -14,11 +14,45 @@ window.navigator.geolocation.getCurrentPosition(function(data){
       console.log(data)
       $('#moods-list').children().remove();
       $('#genres-list').children().remove();
+      $('.mood-genre-buttons').empty();
       var weatherData = data;
       weather(weatherData);
-      renderMoods(moods);
-      renderGenres(genres);
-  })
+      warblrPanel();
+      $('<button>').attr("id", "Change Your Playlist").text("Change Your Playlist").css({
+        "background-color": "white",
+        "border-color": "black",
+        "border-style": "solid",
+        "height": "50px",
+        "width": "70px",
+        "float": "right"
+      }).insertBefore("#zip-text");
+
+      $('<button>').attr("id", "Genre").text("Genre").css({
+        "background-color": "white",
+        "border-color": "black",
+        "border-style": "solid",
+        "height": "60px",
+        "width": "100px",
+        "margin-top": "30px",
+        "margin-left": "100px"
+      }).appendTo('.mood-genre-buttons');
+
+      $('<button>').attr("id", "Mood").text("Mood").css({
+        "background-color": "white",
+        "border-color": "black",
+        "border-style": "solid",
+        "height": "60px",
+        "width": "100px",
+        "margin-top": "30px",
+        "margin-left": "100px"
+      }).appendTo('.mood-genre-buttons');
+
+      // warblrButton("Change Your Playlist", "30px", "60px").insertBefore('#zip-text');
+      // warblrButton("Mood", "60px", "100px").insertAfter('#zip-location');
+      // warblrButton("Genre", "60px", "100px").insertAfter('#Mood');
+      // renderMoods(moods);
+      // renderGenres(genres);
+    })
   },
   function(error){console.log(error)
 });
--- app/assets/javascripts/zip_input.js
+++ app/assets/javascripts/zip_input.js
@@ -17,10 +17,43 @@ $(function(){
           console.log(data)
           $('#moods-list').children().remove();
           $('#genres-list').children().remove();
+          $('.mood-genre-buttons').empty();
           var weatherData = data;
           weather(weatherData);
-          renderMoods(moods);
-          renderGenres(genres);
+          warblrPanel();
+          $('<button>').attr("id", "Change Your Playlist").text("Change Your Playlist").css({
+            "background-color": "white",
+            "border-color": "black",
+            "border-style": "solid",
+            "height": "30px",
+            "width": "60px",
+            "float": "right"
+          }).insertBefore("#zip-text");
+
+          $('<button>').attr("id", "Genre").text("Genre").css({
+            "background-color": "white",
+            "border-color": "black",
+            "border-style": "solid",
+            "height": "60px",
+            "width": "100px",
+            "margin-top": "30px",
+            "margin-left": "100px"
+          }).appendTo('.mood-genre-buttons');
+
+          $('<button>').attr("id", "Mood").text("Mood").css({
+            "background-color": "white",
+            "border-color": "black",
+            "border-style": "solid",
+            "height": "60px",
+            "width": "100px",
+            "margin-top": "30px",
+            "margin-left": "100px"
+          }).appendTo('.mood-genre-buttons');
+
+          // warblrButton("Mood", "60px", "100px").insertAfter('#zip-location');
+          // warblrButton("Genre", "60px", "100px").insertAfter('#Mood');
+          // renderMoods(moods);
+          // renderGenres(genres);
         }
       });
     }
